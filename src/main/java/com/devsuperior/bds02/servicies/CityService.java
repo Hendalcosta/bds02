3cb2e1172866e76f58b0dd76de282d2d6fe3a129
Henderson <hendalcosta@gmail.com>
2022-12-27 19:49:53 -0300

    EventRepository, EventService, CityRepository, CityService created

---
+++ src/main/java/com/devsuperior/bds02/servicies/CityService.java
@@ -0,0 +1,65 @@
+package com.devsuperior.bds02.servicies;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll() {
+		
+		List<City> list = repository.findAll(Sort.by("name"));	
+		
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());	
+	}
+	
+	@Transactional(readOnly = true)
+	public CityDTO findById(Long id) {
+		
+		Optional<City> obj = repository.findById(id);	
+		City entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+		
+		return new CityDTO(entity);
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+
+		}
+		catch (DataIntegrityViolationException e ) {
+		
+		}
+
+	}
+
+}
